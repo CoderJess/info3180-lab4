c9fb2e47f1de191df711e2ab987ab4039e85baa6
Shaquille Thomas <roreythomz26@gmail.com>
2025-03-13 02:46:48 -0500

    Files Updated

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,13 @@ from wtforms.validators import InputRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    file = FileField('Upload File', validators=[DataRequired()])
+    submit = SubmitField('Upload')
+
+    def validate_file(self, file):
+        if file.data:
+            filename = file.data.filename
+            if not (filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png')):
+                raise ValidationError('File must be a JPG or PNG image.')
--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
+from app.forms import UploadForm
 
 
 ###
@@ -24,15 +26,22 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
+    form = UploadForm()
 
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        
+        # Save the file to the upload folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+        return redirect(url_for('upload'))
 
     return render_template('upload.html')
 
@@ -43,20 +52,26 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        # Query the database for a user based on the username
+        user = UserProfile.query.filter_by(username=username).first()
 
-        # Gets user id, load into session
-        login_user(user)
+        if user and check_password_hash(user.password, password):
+            # Gets user id, load into session
+            login_user(user)
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+            # Flash a message to the user
+            flash('You have successfully logged in!', 'success')
+
+            # Redirect the user to the upload form
+            return redirect(url_for("upload"))
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
+
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
